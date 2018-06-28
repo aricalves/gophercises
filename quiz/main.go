3c6d4bab4922471742c33dab41bb283c3dca0fa6
Aric Alves <aric.alves@yahoo.com>
2018-06-28 13:30:00 -0700

    Refactor to use flag pkg

--- quiz/main.go
+++ quiz/main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"bufio"
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"log"
 	"os"
-	"time"
 )
 
 // TODO - remove globals where possible
@@ -16,10 +15,19 @@ var totalQuestions int
 func main() {
 	// TODO - Welcome message, enter name or something
 	fmt.Println("Hello...")
-	// TODO -
+	csvFileName := flag.String("csv", "problems.csv", "CSV file in 'question, answer' format.")
+	flag.Parse()
 
-	f := openCSV("./problems.csv")
-	reader := csv.NewReader(bufio.NewReader(f))
+	f, err := os.Open(*csvFileName)
+	if err != nil {
+		log.Fatalln("Err opening CSV", err)
+	}
+
+	reader := csv.NewReader(f)
+	questions, err := reader.ReadAll()
+	if err != nil {
+		log.Fatalln("Err reading problems", err)
+	}
 
 	// TODO:
 	//		While we are above EOF:
@@ -33,36 +41,5 @@ func main() {
 	//			Display user score and name
 	//			Terminate
 
-	printMatrix(readCSV(reader))
-	rest()
 	fmt.Printf("You scored %v out of %v questions.\n", score, totalQuestions)
-}
-
-func openCSV(target string) *os.File {
-	f, err := os.Open(target)
-	if err != nil {
-		log.Fatalln("Err opening CSV", err)
-	}
-	return f
-}
-
-func readCSV(reader *csv.Reader) [][]string {
-	data, err := reader.ReadAll()
-	totalQuestions = len(data)
-	if err != nil {
-		log.Fatalln("Error reading problems", err)
-	}
-	return data
-}
-
-func printMatrix(m [][]string) {
-	for _, s := range m {
-		for _, item := range s {
-			fmt.Println(item)
-		}
-	}
-}
-
-func rest() {
-	time.Sleep(time.Duration(500) * time.Millisecond)
 }
