20dc266859b4346273003f50c4f7c0ed7e73f59a
Aric Alves <aric.alves@yahoo.com>
2018-06-29 00:03:53 -0700

    Refactor yaml and json handlers

--- urlshort/handler.go
+++ urlshort/handler.go
@@ -7,13 +7,15 @@ import (
 	yaml "gopkg.in/yaml.v2"
 )
 
+type pathMap map[string]string
+
 // MapHandler will return an http.HandlerFunc (which also
 // implements http.Handler) that will attempt to map any
 // paths (keys in the map) to their corresponding URL (values
 // that each key in the map points to, in string format).
 // If the path is not provided in the map, then the fallback
 // http.Handler will be called instead.
-func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+func MapHandler(pathsToUrls pathMap, fallback http.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		path, ok := pathsToUrls[r.URL.Path]
 		if ok {
@@ -22,6 +24,23 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 			fallback.ServeHTTP(w, r)
 		}
 	}
+}
+
+func makePathMap(in []pathMap) pathMap {
+	m := make(pathMap)
+	for _, line := range in {
+		m[line["path"]] = line["url"]
+	}
+	return m
+}
+
+type unmarshaler func([]byte, interface{}) error
+
+func unmarshal(in []byte, fn unmarshaler) (out []pathMap) {
+	if err := fn(in, &out); err != nil {
+		panic(err)
+	}
+	return out
 }
 
 // JSONHandler will parse the provided JSON and then return
@@ -41,29 +60,8 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 // 	  }
 // 	]
 //
-func JSONHandler(json []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	return MapHandler(jtom(parseJSON(json)), fallback), nil
-}
-
-type path struct {
-	Path string `json:"path"`
-	URL  string `json:"url"`
-}
-
-func parseJSON(s []byte) []path {
-	var paths []path
-	if err := json.Unmarshal(s, &paths); err != nil {
-		panic(err)
-	}
-	return paths
-}
-
-func jtom(in []path) map[string]string {
-	des := make(map[string]string)
-	for _, line := range in {
-		des[line.Path] = line.URL
-	}
-	return des
+func JSONHandler(jsn []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	return MapHandler(makePathMap(unmarshal(jsn, json.Unmarshal)), fallback), nil
 }
 
 // YAMLHandler will parse the provided YAML and then return
@@ -83,21 +81,5 @@ func jtom(in []path) map[string]string {
 // See MapHandler to create a similar http.HandlerFunc via
 // a mapping of paths to urls.
 func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	return MapHandler(ytom(parseYAML(yml)), fallback), nil
-}
-
-func parseYAML(in []byte) (out []map[string]string) {
-	err := yaml.Unmarshal(in, &out)
-	if err != nil {
-		panic(err)
-	}
-	return out
-}
-
-func ytom(y []map[string]string) map[string]string {
-	paths := make(map[string]string)
-	for _, line := range y {
-		paths[line["path"]] = line["url"]
-	}
-	return paths
+	return MapHandler(makePathMap(unmarshal(yml, yaml.Unmarshal)), fallback), nil
 }
