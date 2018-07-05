07f56d031b556ffcfab16878b5b5d5afef29ff1e
Aric Alves <aric.alves@yahoo.com>
2018-07-05 10:42:35 -0700

    Adds server, router, JSON parsing; serves JSON

---
+++ cyoa/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+)
+
+var story string
+
+func main() {
+	flag.StringVar(&story, "story", "./public/gopher.json", "Location of JSON story")
+	flag.Parse()
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", hello)
+
+	http.Handle("/", r)
+
+	port := ":8080"
+	log.Printf("Serving from localhost%v", port)
+	log.Fatal(http.ListenAndServe(port, nil))
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	w.Write(parseJSONStory())
+}
+
+func parseJSONStory() []byte {
+	r, err := os.Open(story)
+	d, err := ioutil.ReadAll(r)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return d
+}
